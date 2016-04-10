c55a2f3d32bd26897bfc735ed7f8be6ae4b2c6a9
yifaan <yfyang0814@gmail.com>
2016-04-10 07:13:51 -0400

    a working edition

--- main.cpp
+++ main.cpp
@@ -18,6 +18,7 @@
 #include <cmath>
 #include <limits>
 #include <time.h>
+#include <algorithm>
 #include "RobotDefinitions.h"
 #include "LocalizationController.h"
 #include "main.h"
@@ -30,11 +31,26 @@
  */
 void getRobotPositionEstimate(RobotState& estimatePosn)
 {
-    // TODO: Write your procedures to set the current robot position estimate here
+    // the position would be the particle that has maximum weight
+    std::vector<double>::iterator it = std::max_element(g_weights.begin(),g_weights.end()); 
+    size_t ind = std::distance(g_weights.begin(), it);
     
-//    estimatePosn.x = 0.0;
-//    estimatePosn.y = 0.0;
-//    estimatePosn.theta = 0.0;
+    estimatePosn.x = g_particles[ind].x;
+    estimatePosn.y = g_particles[ind].y;
+    estimatePosn.theta = g_particles[ind].theta;
+
+    // calculate the number of efficient particles
+    double sum=0, sq_sum = 0;
+    for (int i = 0; i < NUM_PARTICLES; i++) {
+        sum += g_weights[i];
+        sq_sum += g_weights[i]*g_weights[i];
+    }
+    double n_eff = sum * sum / sq_sum;
+    //if number of efficient particles are less than an threshold, we need to resample the particles
+    if (n_eff < 0.4 * NUM_PARTICLES) {
+        g_particles = resample(g_particles, g_weights);
+        std::cout << "resample!!" << std::endl;
+    }
 }
 
 /**
@@ -53,15 +69,16 @@ void motionUpdate(RobotState delta)
     double dtrans = sqrt(delta.x * delta.x + delta.y * delta.y);
     double drot = delta.theta;
     // get the noise rotational and translational variance
-    double rotVar =  g_robotParams.odom_noise_rotation_from_rotation * drot
-                    +g_robotParams.odom_noise_rotation_from_translation * dtrans;
-    double tranVar = g_robotParams.odom_noise_translation_from_rotation * drot
-                    +g_robotParams.odom_noise_translation_from_translation * dtrans;
+    double rot_var =  (g_robotParams.odom_noise_rotation_from_rotation * fabs(drot)
+                    +g_robotParams.odom_noise_rotation_from_translation * fabs(dtrans));
+    double tran_var = (g_robotParams.odom_noise_translation_from_rotation * fabs(drot)
+                    +g_robotParams.odom_noise_translation_from_translation * fabs(dtrans));
+    
     for (int i = 0; i < NUM_PARTICLES; i++) {
         // for each particle, add noice to its odometry;
-        double dx_r = generateGaussianNoise(delta.x, tranVar);
-        double dy_r = generateGaussianNoise(delta.y, tranVar);
-        double dtheta_r = generateGaussianNoise(delta.theta, rotVar);
+        double dx_r = generateGaussianNoise(delta.x, tran_var);
+        double dy_r = generateGaussianNoise(delta.y, tran_var);
+        double dtheta_r = generateGaussianNoise(delta.theta, rot_var);
         // transform it from robot coordinate to world coordinate
         double theta = g_particles[i].theta;
         double dx_w = cos(theta) * dx_r - sin(theta) * dy_r;
@@ -71,6 +88,9 @@ void motionUpdate(RobotState delta)
         g_particles[i].x += dx_w;
         g_particles[i].y += dy_w;
         g_particles[i].theta += dtheta_w;
+        if (g_particles[i].theta > PI)  g_particles[i].theta -= 2*PI;
+        if (g_particles[i].theta < -PI)  g_particles[i].theta += 2*PI;
+
     }
 }
 
@@ -83,9 +103,64 @@ void motionUpdate(RobotState delta)
  */
 void sensorUpdate(std::vector<MarkerObservation> observations)
 {
-    // TODO: Write your sensor update procedures here
+    // each particle will have an assumption, and the probability
+    // of that assumption is calculated to update the weight
 
-    
+    // correlation
+    double corr[NUM_PARTICLES] = {1}; 
+
+    // loop through marker
+    for (int i = 0; i < observations.size(); i++) {
+        // get the marker position
+        double m_x = g_markerLocations[observations[i].markerIndex].x;
+        double m_y = g_markerLocations[observations[i].markerIndex].y;
+        // get the distance and orientation
+        double m_dist = observations[i].distance, m_ori = observations[i].orientation;
+        // get the variance of sensor noise;
+        double sen_var = (g_robotParams.sensor_noise_distance * fabs(m_dist) +
+                        g_robotParams.sensor_noise_orientation * fabs(m_ori));
+
+        //loop through particles
+        for (int j = 0; j < NUM_PARTICLES; j++) {
+            // get the position of particle
+            double p_x = g_particles[j].x;
+            double p_y = g_particles[j].y;
+            double p_theta = g_particles[j].theta;
+            // get the relative landmark position in robot coordinate
+            double dx_r = m_dist * cos(m_ori);
+            double dy_r = m_dist * sin(m_ori);
+            // transfer it to the world coordinate
+            double dx_w = cos(p_theta) * dx_r - sin(p_theta) * dy_r;
+            double dy_w = sin(p_theta) * dx_r + cos(p_theta) * dy_r;
+            // get the assumption of marker position
+            double m_x_as = p_x + dx_w;
+            double m_y_as = p_y + dy_w;
+            // calculate the probability p(o|s);
+            double dis = ((m_x - m_x_as) * (m_x - m_x_as) + (m_y - m_y_as) * (m_y - m_y_as));
+            double p = exp(-dis / (2.0 * sen_var));
+            g_weights[j] *= p;
+        } 
+    }
+    normalizeWeight(g_weights);
+}
+
+/**
+ * normalizeWeight()
+ * This function is used to normalize weight
+ */
+void normalizeWeight(std::vector<double>& weight) {
+    double min_val = *std::min_element(weight.begin(),weight.end());
+    //shift and calculate sum
+    double sum = 0;
+    for (int i = 0; i < weight.size(); i++) {
+        if (min_val <= 0) weight[i] = weight[i] - min_val + pow(10,-6);
+        sum += weight[i];
+    }
+
+    for (int i = 0; i < weight.size(); i++) {
+        weight[i] /= sum;
+        //printf("%.3f\n", weight[i]);
+    }
 }
 
 /**
@@ -99,9 +174,10 @@ void myinit(RobotState robotState, RobotParams robotParams,
 {
     // initialize particles and 
     //initialize importance weights to be uniformly distributed
+
     for (int i = 0; i < NUM_PARTICLES; i++) {
         g_particles.push_back(robotState);
-        g_weight.push_back(1.0 / NUM_PARTICLES);
+        g_weights.push_back(1.0 / NUM_PARTICLES);
     }
     // intialize robot parameters and landmarks location
     g_robotParams = robotParams;
@@ -115,24 +191,22 @@ void myinit(RobotState robotState, RobotParams robotParams,
  */
 void mydisplay()
 {
-    // TODO: Write your drawing procedures here 
-    //       (e.g., robot position uncertainty representation)
-    
-    
-   // Example drawing procedure
+   // draw all particles
    int pixelX, pixelY;
-   double globalX = g_particles[0].x, globalY = g_particles[0].y;
-   const double POINT_SPREAD = 0.2;
+
+   double globalX, globalY;
    
    // Draw cyan colored points at specified global locations on field
    glBegin(GL_POINTS);
    glColor3f(0.0, 1.0, 1.0);
    
-   global2pixel(globalX, globalY, pixelX, pixelY);
-   glVertex2i(pixelX, pixelY);
-   
+   for (int i = 0; i < NUM_PARTICLES; i++) {
+        globalX = g_particles[i].x;
+        globalY = g_particles[i].y;
+        global2pixel(globalX, globalY, pixelX, pixelY);
+        glVertex2i(pixelX, pixelY);
+   }
    glEnd();
-
 }
 
 /**
@@ -181,6 +255,33 @@ double generateGaussianNoise(double mu, double sigma)
     z0 = sqrt(-2.0 * log(u1)) * cos(two_pi * u2);
     z1 = sqrt(-2.0 * log(u1)) * sin(two_pi * u2);
     return z0 * sigma + mu;
+}
+
+/**
+*resample()
+*use Low Variance Sampling technique to resample the particles
+*/
+std::vector<RobotState> resample(std::vector<RobotState>& particles, std::vector<double>& weights) {
+
+    printf("\n");
+    std::vector<RobotState> new_particles;
+
+    double increment = 1.0/NUM_PARTICLES;
+    double val = rand() * (1.0 / RAND_MAX);
+    int ind = 0;
+    for (int i=0; i<NUM_PARTICLES;i++) {
+        val += increment;
+        while (weights[ind] < val) {
+            val -= weights[ind++];
+            if (ind >= NUM_PARTICLES) ind = 0;
+        }
+        
+        new_particles.push_back(particles[ind]);
+    }
+
+    // set weights to uniform distributed
+    std::fill(weights.begin(), weights.end(), 1.0 / NUM_PARTICLES);
+    return new_particles;
 }
 
 
--- main.h
+++ main.h
@@ -29,12 +29,21 @@ int  mykeyboard(unsigned char key);
 //generateGaussianNoise
 double generateGaussianNoise(double, double);
 
+//normalize weight
+void normalizeWeight(std::vector<double>&);
+
+//resemple particles
+std::vector<RobotState> resample(std::vector<RobotState>&, std::vector<double>&);
+
 // Set the global variables
-const int NUM_PARTICLES = 10;
+const int NUM_PARTICLES = 1000;
+const double PI = 3.14159;
+
 // particles
 std::vector<RobotState> g_particles;
+
 // importance weight
-std::vector<double> g_weight;
+std::vector<double> g_weights;
 RobotParams g_robotParams;
 FieldLocation* g_markerLocations;
 
